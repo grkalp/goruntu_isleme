e1a79e99862155170f4008f615fd42218e31b63a
grkalp <alper@garagebilisim.com>
2021-01-04 17:44:26 +0300

    arrange

--- 9-Projeler/1-renksay.py
+++ 9-Projeler/1-renksay.py
@@ -1,23 +1,38 @@
 import cv2
 import numpy as np
 
-# alt_Renk = np.array([30, 60, 60])
-# ust_Renk = np.array([64, 255, 255])
+# alt_Renk = (30, 60, 60)
+# ust_Renk = (64, 255, 255)
 # RENK = 'YESIL'
 
-# alt_Renk = (10, 100, 100)
-# ust_Renk = (40, 255, 255)
-# RENK = 'SARI'
+# 1. deneme
+alt_Renk = (10, 100, 100)
+ust_Renk = (40, 255, 255)
 
-alt_Renk = (170, 100, 100)
-ust_Renk = (190, 255, 255)
-RENK = 'KIRMIZI'
+# 2.deneme
+# alt_Renk = (20, 100, 100)
+# ust_Renk = (30, 255, 255)
+# 3.deneme
+# alt_Renk = (23, 41, 133)
+# ust_Renk = (40, 150, 255)
+# 4.deneme
+# alt_Renk = (0, 166, 181)
+# ust_Renk = (240, 255, 255)
+# 5.deneme
+# alt_Renk = (0, 162, 186)
+# ust_Renk = (46, 242, 255)
+RENK = 'SARI'
+
+# alt_Renk = (170, 100, 100)
+# ust_Renk = (190, 255, 255)
+# RENK = 'KIRMIZI'
 
 # alt_Renk = (75, 100, 100)
 # ust_Renk = (130, 255, 255)
 # RENK = 'Mavi'
 
-kamera = cv2.VideoCapture(0)
+kamera = cv2.VideoCapture("http://admin:03120216Ga@192.168.0.20/video/mjpg.cgi")
+# kamera = cv2.VideoCapture(0)
 cember = True
 
 while True:
@@ -34,7 +49,8 @@ while True:
     say = 0
     for kontur in konturlar:
         alan = cv2.contourArea(kontur)
-        if alan > 600:
+        # if alan > 600:
+        if (alan > 50) & (alan < 100):
             say += 1
             (x, y, w, h) = cv2.boundingRect(kontur)
             cv2.rectangle(kare, (x, y), (x+w, y+h), (0, 255, 0), 2)
